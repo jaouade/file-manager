cb42b7a66d2f566e17da266ce663afe476b2dc7f
ynajeh <ynajeh@pictet-technologies.com>
2019-11-07 07:42:37 +0100

    add tomcat dependency to run in a dedicated tomcat instance

--- src/main/java/com/filemanagement/app/FileManagementApplication.java
+++ src/main/java/com/filemanagement/app/FileManagementApplication.java
@@ -3,12 +3,19 @@ package com.filemanagement.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class FileManagementApplication {
+public class FileManagementApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FileManagementApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FileManagementApplication.class);
     }
 
 }
